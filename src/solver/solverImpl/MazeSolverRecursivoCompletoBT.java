00ef5caedbe92136a1c3f066fb8287f5a8eb0932
Davesoo <dsiguenzav@est.ups.edu.ec>
2025-07-27 23:10:30 -0500

    Cambios

--- src/solver/solverImpl/MazeSolverRecursivoCompletoBT.java
+++ src/solver/solverImpl/MazeSolverRecursivoCompletoBT.java
@@ -1,5 +1,57 @@
 package solver.solverImpl;
 
-public class MazeSolverRecursivoCompletoBT {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import models.*;
+import solver.MazeSolver;
+
+public class MazeSolverRecursivoCompletoBT implements MazeSolver{
+
+    private List<Cell> path;
+    private boolean[][] visited;
+
+    @Override
+    public SolveResults solve(Cell[][] maze, Cell start, Cell end) {
+        long startTime = System.nanoTime();
+        path = new ArrayList<>();
+        visited = new boolean[maze.length][maze[0].length];
+
+        boolean found = findPath(maze, start.getRow(), start.getCol(), end);
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        
+        if (found) {
+            Collections.reverse(path);
+        } else {
+            path.clear();
+        }
+
+        String mazeSize = maze.length + "x" + maze[0].length;
+        AlgorithmResult algoResult = new AlgorithmResult("Recursivo (4 dir + BT)", path.size(), duration, mazeSize);
+        return new SolveResults(path, algoResult);
+    }
+
+    private boolean findPath(Cell[][] maze, int r, int c, Cell end) {
+        if (r < 0 || c < 0 || r >= maze.length || c >= maze[0].length || visited[r][c] || maze[r][c].getState() == CellState.WALL) {
+            return false;
+        }
+        
+        visited[r][c] = true;
+
+        if (maze[r][c].equals(end)) {
+            path.add(maze[r][c]);
+            return true;
+        }
+
+        if (findPath(maze, r + 1, c, end)) { path.add(maze[r][c]); return true; }
+        if (findPath(maze, r - 1, c, end)) { path.add(maze[r][c]); return true; }
+        if (findPath(maze, r, c + 1, end)) { path.add(maze[r][c]); return true; }
+        if (findPath(maze, r, c - 1, end)) { path.add(maze[r][c]); return true; }
+
+       
+        return false;
+    }
     
 }
